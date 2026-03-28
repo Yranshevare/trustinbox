80f45c59a24d461ae29a25086d691e75a02c8dc5
r2hu1 <newasram86@gmail.com>
2026-03-28 17:36:58 +0530

    clean

--- turbo/generators/config.ts
+++
@@ -1,9 +0,0 @@
-import type { PlopTypes } from '@turbo/gen';
-
-import { createPackageGenerator } from './templates/package/generator';
-
-const generators = [createPackageGenerator];
-
-export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  generators.forEach((gen) => gen(plop));
-}
